791a6df65dc4e26ac046232884db41c1e3c5b371
insanniity <cleydson.cg@live.com>
2021-01-21 10:43:44 -0300

    [FrontWeb] Add NavBar

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,8 +1,11 @@
 import React from 'react';
+import './core/assets/styles/custom.scss';
+import './app.scss';
+import Navbar from './core/components/NavBar';
 
 const App = () => {
     return (
-        <h1>hello</h1>
+        <Navbar />
     )    
     
 }
