e3ed794691166b3a6ee09d86e1fb8179acc3f6c7
insanniity <cleydson.cg@live.com>
2021-01-21 17:36:34 -0300

    [FrontWeb] Products details page

--- front-web/src/pages/Catalog/components/ProductDetails/index.tsx
+++ front-web/src/pages/Catalog/components/ProductDetails/index.tsx
@@ -1,5 +1,8 @@
 import React from 'react';
-import { useParams } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
+import { ReactComponent as ArrowIcon } from '../../../../core/assets/images/arrow.svg';
+import { ReactComponent as ProductImage } from '../../../../core/assets/images/product.svg';
+import ProductPrice from '../../../../core/components/ProductPrice';
 import './styles.scss';
 
 type ParamsType = {
@@ -9,11 +12,33 @@ type ParamsType = {
 const ProductDetails = () => {
     const { productsId } = useParams<ParamsType>();
     console.log(productsId);
-    return(
+    return (
         <div className="prduct-details-container">
-            ProductDetails
+            <div className="product-details card-base border-radius-20">
+                <Link to="/products" className="product-details-goback">
+                    <ArrowIcon className="icon-goback" />
+                    <h1 className="text-goback">Voltar</h1>
+                </Link>
+                <div className="row">
+                    <div className="col-6 pr-5">
+                        <div className="product-details-card text-center">
+                            <ProductImage className="product-details-image" />
+                        </div>
+                        <h1 className="product-details-name">
+                            Computador Desktop - Intel Core i7
+                        </h1>
+                        <ProductPrice price="2.779,00"/>                        
+                    </div>
+                    <div className="col-6 product-details-card">
+                        <h1 className="product-description-title">Descrição do produto</h1>
+                        <p className="product-description-text">
+                            Seja um mestre em multitarefas com a capacidade para exibir quatro aplicativos simultâneos na tela. A tela está ficando abarrotada? Crie áreas de trabalho virtuais para obter mais espaço e trabalhar com os itens que você deseja. Além disso, todas as notificações e principais configurações são reunidas em uma única tela de fácil acesso.
+                        </p>
+                    </div>
+                </div>
+            </div>
         </div>
-    );    
+    );
 };
 
 export default ProductDetails;
