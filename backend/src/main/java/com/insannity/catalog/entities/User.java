a8991f06bb1e36f6a3eb651d6c4267f5e23fc500
insanniity <cleydson.cg@live.com>
2021-02-08 16:29:31 -0300

    [BackEnd] User Resources

--- backend/src/main/java/com/insannity/catalog/entities/User.java
+++ backend/src/main/java/com/insannity/catalog/entities/User.java
@@ -4,6 +4,7 @@ import java.io.Serializable;
 import java.util.HashSet;
 import java.util.Set;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
@@ -27,6 +28,7 @@ public class User implements Serializable{
 	
 	private String firstName;
 	private String lastName;
+	@Column(unique = true)
 	private String email;
 	private String password;
 	
