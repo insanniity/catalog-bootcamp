cdd7217efb97eef9cac50169188aa28300f01fb2
insanniity <cleydson.cg@live.com>
2021-02-10 22:48:58 -0300

    [BackEnd] OAuth2 authorization server

--- backend/src/main/java/com/insannity/catalog/resources/exceptions/ValidationError.java
+++ backend/src/main/java/com/insannity/catalog/resources/exceptions/ValidationError.java
@@ -5,6 +5,8 @@ import java.util.List;
 
 public class ValidationError extends StandardError{
 	
+	
+	private static final long serialVersionUID = 1L;
 	private List<FieldMessage> errors = new ArrayList <>();
 
 	public List<FieldMessage> getErrors() {
