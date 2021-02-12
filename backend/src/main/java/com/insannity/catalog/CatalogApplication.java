3b19017c98dd333949208c4bd523f933174e852b
insanniity <cleydson.cg@live.com>
2021-02-12 16:40:30 -0300

    [BackEnd] Upload test

--- backend/src/main/java/com/insannity/catalog/CatalogApplication.java
+++ backend/src/main/java/com/insannity/catalog/CatalogApplication.java
@@ -1,13 +1,28 @@
 package com.insannity.catalog;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class CatalogApplication {
+import com.insannity.catalog.services.S3Service;
 
+@SpringBootApplication
+public class CatalogApplication implements CommandLineRunner{
+	
+	@Autowired
+	private S3Service s3Service;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CatalogApplication.class, args);
 	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		s3Service.uploadFile("C:\\Users\\insannity\\Pictures\\Screenshot_1.png");
+		
+	}
+	
+	
 
 }
