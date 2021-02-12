c43d3346f5539234351f4ce53ae37ae09db89ac0
insanniity <cleydson.cg@live.com>
2021-02-12 15:37:04 -0300

    [BackEnd]  fix search compatibility postgrees

--- backend/src/main/java/com/insannity/catalog/repositories/ProductRepository.java
+++ backend/src/main/java/com/insannity/catalog/repositories/ProductRepository.java
@@ -1,5 +1,7 @@
 package com.insannity.catalog.repositories;
 
+import java.util.List;
+
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -12,6 +14,6 @@ import com.insannity.catalog.entities.Product;
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long>{
 	
-	@Query("SELECT DISTINCT obj FROM Product obj INNER JOIN obj.categories cats WHERE (:category IS NULL OR :category IN cats) AND (LOWER(obj.name) LIKE LOWER(CONCAT('%',:name,'%')))")
-	Page<Product> find (Category category,String name, Pageable pageble);
+	@Query("SELECT DISTINCT obj FROM Product obj INNER JOIN obj.categories cats WHERE (COALESCE(:categories) IS NULL OR cats IN :categories) AND (LOWER(obj.name) LIKE LOWER(CONCAT('%',:name,'%')))")
+	Page<Product> find (List<Category> categories ,String name, Pageable pageble);
 }
