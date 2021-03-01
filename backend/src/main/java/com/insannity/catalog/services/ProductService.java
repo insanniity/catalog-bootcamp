3c5ccdbd22b8831dbb5507a7f8fa623502435b78
insanniity <cleydson.cg@live.com>
2021-03-01 08:47:27 -0300

    [BackEnd] Fixes

--- backend/src/main/java/com/insannity/catalog/services/ProductService.java
+++ backend/src/main/java/com/insannity/catalog/services/ProductService.java
@@ -58,11 +58,7 @@ public class ProductService {
 	@Transactional
 	public ProductDTO insert(ProductDTO dto) {
 		Product entity = new Product();
-		copyDtoToEntity(dto, entity);
-		if(entity.getCategories().size()== 0) {
-			Category cat = categoryRepository.getOne(1L);
-			entity.getCategories().add(cat);
-		}
+		copyDtoToEntity(dto, entity);		
 		entity = repository.save(entity);
 		return new ProductDTO(entity);
 		
@@ -72,11 +68,7 @@ public class ProductService {
 	public ProductDTO update(Long id, ProductDTO dto) {
 		try{
 			Product entity = repository.getOne(id);		
-			copyDtoToEntity(dto, entity);
-			if(entity.getCategories().size()== 0) {
-				Category cat = categoryRepository.getOne(1L);
-				entity.getCategories().add(cat);
-			}
+			copyDtoToEntity(dto, entity);			
 			entity = repository.save(entity);
 			return new ProductDTO(entity);
 		}catch(EntityNotFoundException e) {
