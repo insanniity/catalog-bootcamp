16985f2c21afc39fb12ee56fb7c274718b2d1fc2
insanniity <cleydson.cg@live.com>
2021-03-04 17:51:00 -0300

    [FrontMobile] imports

--- front-mobile/src/routes/index.tsx
+++ front-mobile/src/routes/index.tsx
@@ -1,16 +1,28 @@
 import { createStackNavigator } from '@react-navigation/stack';
 import React from 'react';
-import { Catalog, Home } from '../pages';
+import { Catalog, Home, ProductDetails } from '../pages';
+import { colors, navBar } from '../styles';
+import { Text} from 'react-native';
+import { NavBar } from '../components';
 
 
 const Stack = createStackNavigator();
 
+const HeaderText: React.FC = () => <Text style={navBar.leftText}>Catalog</Text>;
 
 const Routes: React.FC =() =>{
     return(
-        <Stack.Navigator>
+        <Stack.Navigator screenOptions={{
+            headerTitle: " ",
+            headerStyle:{
+                backgroundColor: colors.primary,
+            },
+            headerLeft: () => <HeaderText />,
+            headerRight:() => <NavBar />,
+        }}>
             <Stack.Screen name="Home" component={Home} />
             <Stack.Screen name="Catalog" component={Catalog} />
+            <Stack.Screen name="ProductDetails" component={ProductDetails}/>
         </Stack.Navigator>
     )
 }
